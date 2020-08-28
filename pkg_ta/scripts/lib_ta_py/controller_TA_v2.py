f1eb74a8bd495374015add38a7b0f336beab4e8d
adamsyah <fadillahadam11@gmail.com>
2020-08-28 07:01:55 +0700

    Make the EDIT_WAYPOINTS notebook to automatically update all of the set of waypoints. Add some new codes

---
+++ pkg_ta/scripts/lib_ta_py/controller_TA_v2.py
@@ -0,0 +1,201 @@
+# This code is written in Python 3 environment
+
+import numpy as np
+from numba import njit, float64, int64
+from numba.experimental import jitclass
+
+# Stanley Controller & PID Controller with Feed Forward Term
+
+#########################################################################################
+# VERSION 1
+
+spec_v1 = [('_kp', float64), ('_ki', float64), ('_kd', float64), ('_ff_params', float64[:]),
+           ('_sat_long_max', float64), ('_sat_long_min', float64),
+           ('_ev', float64), ('_ev_last', float64), ('_ev_sum', float64),
+           ('_ev_sum_max', float64), ('_ev_sum_min', float64),
+           ('_ks', float64), ('_kv', float64), ('_l', float64),
+           ('_dead_band', float64), ('_sat_lat_max', float64),
+           ('_sat_lat_min', float64), ('_e_lat', float64), ('_e_yaw', float64),
+           ('_waypoints', float64[:, :]), ('_closest_idx', int64),
+           ('_max_throttle', float64), ('_min_throttle', float64),
+           ('_kv_yaw', float64), ('_kv_lat', float64), ('_kv_throttle', float64),
+           ('_elat_last', float64),
+           ('_kp_lat', float64), ('_kd_lat', float64),]
+
+@jitclass(spec_v1)
+class Controller_v1(object):
+    def __init__(self, kp, ki, kd, feed_forward_params, sat_long,\
+                 ks, kv, length, lateral_dead_band, sat_lat,\
+                 waypoints,\
+                 max_throttle, min_throttle, kv_yaw, kv_lat, kv_throttle,\
+                 kp_lat, kd_lat):
+        # In this version, the integral term will be clamped based on the
+        # saturation value and the feed-forward term
+
+        # The parameters of the longitudinal controller
+        self._kp = kp
+        self._ki = ki
+        self._kd = kd
+        self._ff_params = feed_forward_params
+        self._sat_long_max = max(sat_long[0], sat_long[1])
+        self._sat_long_min = min(sat_long[0], sat_long[1])
+        self._ev = 0.
+        self._ev_sum = 0.
+        self._ev_sum_max = 0. # This value will be updated in each iteration
+        self._ev_sum_min = 0. # This value will be updated in each iteration
+
+        # The parameters of the lateral controller
+        self._ks = ks
+        self._kv = kv
+        self._l = length
+        self._dead_band = lateral_dead_band
+        self._sat_lat_max = np.fmax(sat_lat[0], sat_lat[1])
+        self._sat_lat_min = np.fmin(sat_lat[0], sat_lat[1])
+        self._e_lat = 0.
+        self._elat_last = 0.
+        self._e_yaw = 0.
+
+        # Waypoints (n, 5) -> x, y, yaw, v, curvature
+        self._waypoints = waypoints
+        self._closest_idx = 0
+
+        # Additional Throttle Control (UNTUK TA)
+        self._max_throttle = max_throttle
+        self._min_throttle = min_throttle
+        self._kv_yaw = kv_yaw
+        self._kv_lat = kv_lat
+        self._kv_throttle = kv_throttle
+        self._kp_lat = kp_lat
+        self._kd_lat = kd_lat
+
+    def update_waypoints(self, new_waypoints):
+        self._waypoints = new_waypoints
+
+    def reset_integral_derivative(self):
+        self._ev_sum = 0.0
+        self._ev_last = 0.0
+
+    def get_error(self):
+        return self._ev, self._e_lat, self._e_yaw
+
+    def get_closest_index(self):
+        return self._closest_idx
+
+    def get_instantaneous_setpoint(self):
+        return self._waypoints[self._closest_idx]
+
+    def _update_error(self, x, y, v, yaw):
+        # Waypoints (n, 5) -> x, y, yaw, v, curvature
+
+        # Find the closest waypoint
+        self._closest_idx = np.argmin(np.sum(np.square(self._waypoints[:, :2] - np.array([x, y])), axis=-1))
+
+        # Find the yaw error
+        self._e_yaw = self._waypoints[self._closest_idx, 2] - yaw
+        self._e_yaw = (self._e_yaw + np.pi) % (2 * np.pi) - np.pi # Wrap the angle to [-pi, pi)
+
+        # Find the lateral or crosstrack error
+        if self._closest_idx == 0:
+            idx = 1
+        else:
+            idx = self._closest_idx
+        y2 = self._waypoints[idx, 1]
+        x2 = self._waypoints[idx, 0]
+        y1 = self._waypoints[idx - 1, 1]
+        x1 = self._waypoints[idx - 1, 0]
+        dy = y2 - y1
+        dx = x2 - x1
+        c = dx*y1 - dy*x1
+        self._e_lat = (dy*x + c - dx*y) \
+                        / (np.sqrt(dx**2 + dy**2) + 10**(-32))
+
+        # Find the velocity error
+        self._ev = self._waypoints[self._closest_idx, 3] - v
+
+    def _feed_forward_longitudinal(self, v):
+        if v < 0.:
+            return 0.
+        else:
+            return self._ff_params[0] * (1. - np.exp(- self._ff_params[1] * v))
+
+    def _feed_forward_lateral(self):
+        temp = self._l * self._waypoints[self._closest_idx, -1]
+        if np.abs(temp) > 1.:
+            temp = np.sign(temp)
+        return np.fmax(np.fmin(np.arcsin(temp), self._sat_lat_max), self._sat_lat_min) # From -pi/2 to pi/2
+
+    # def _feed_forward_lateral(self):
+    #     temp = self._l * self._waypoints[self._closest_idx, -1]
+    #     return np.fmax(np.fmin(np.arctan(temp), self._sat_lat_max), self._sat_lat_min) # From -pi/2 to pi/2
+
+    def calculate_control_signal(self, dt, x, y, v, yaw):
+        # Waypoints (n, 5) -> x, y, yaw, v, curvature
+        self._update_error(x, y, v, yaw)
+
+        # Longitudinal control
+
+        # PID Control
+        # ff_long = self._feed_forward_longitudinal(self._waypoints[self._closest_idx, 3])
+        #
+        # ev_dot = (self._ev - self._ev_last) / dt
+        #
+        # self._ev_sum_max = np.fmax((self._sat_long_max - ff_long)/self._ki, 0.)
+        # self._ev_sum_min = np.fmin((self._sat_long_min - ff_long)/self._ki, 0.)
+        # self._ev_sum = self._ev_sum + self._ev * dt
+        # self._ev_sum = np.fmax(np.fmin(self._ev_sum, self._ev_sum_max), self._ev_sum_min)
+        #
+        # cs_long = ff_long +\
+        #             self._kp * self._ev +\
+        #             self._ki * self._ev_sum +\
+        #             self._kd * ev_dot
+        # cs_long = np.fmax(np.fmin(cs_long, self._sat_long_max), self._sat_long_min)
+        #
+        # self._ev_last = self._ev
+
+        # Throttle Control
+        e_lat_abs = np.abs(self._e_lat)
+        e_yaw_abs = np.abs(self._e_yaw)
+        # cs_long = self._max_throttle / (1. + self._kv_yaw*e_yaw_abs + self._kv_lat*e_lat_abs)
+        # cs_long = self._max_throttle / (1. + self._kv_lat*e_lat_abs)
+        cs_long = self._waypoints[self._closest_idx, 3] / (1. + self._kv_lat * e_lat_abs**2)
+        cs_long = np.fmax(cs_long, self._min_throttle)
+
+        # Lateral control
+        # temp = 0.0
+        # if np.abs(self._e_lat) > self._dead_band:
+        #    temp = self._e_lat
+
+        a = self._feed_forward_lateral()
+        # b = self._e_yaw * 7.5
+        b = 0.
+        # c = np.arctan(self._ks * temp / (self._kv + v))
+        # c = np.arctan(self._ks * self._e_lat / (self._kv + v))
+        # c = np.arctan(self._ks * self._e_lat / (self._kv + cs_long*self._kv_throttle))
+        # kpp = 15. * np.pi / 180.
+        # kdd = 30. * np.pi / 180.        
+        c = self._kp_lat * self._e_lat + self._kd_lat * (self._e_lat - self._elat_last)/dt
+        d = a + b + c
+
+        cs_lat = max(min(d, self._sat_lat_max), self._sat_lat_min)
+
+        self._elat_last = self._e_lat
+
+        return cs_long, cs_lat
+
+print("Compilling the Controller_v1 ...")
+controller = Controller_v1(0.5, 0.1, 0.1, np.array([1., 2.]),
+                           np.array([-1., 1.]), 2.0, 0.1, 2.5,
+                           0.01, np.array([-np.pi/3., np.pi/3.]),
+                           np.random.randn(100, 5),
+                           0.2, 0.08, 0.75, 0.75, 15.0,
+                           15. * np.pi / 180., 30. * np.pi / 180.)
+controller.update_waypoints(np.random.randn(100, 5))
+controller.reset_integral_derivative()
+_ = controller.get_error()
+_ = controller.get_instantaneous_setpoint()
+controller._update_error(0., 0., 1.0, 0.)
+_ = controller._feed_forward_longitudinal(2.5)
+_ = controller._feed_forward_lateral()
+_ = controller.calculate_control_signal(0.01, 0., 0., 1.0, 0.)
+print("The Controller_v1 class has been compiled !")
+#########################################################################################
---
+++ pkg_ta/scripts/v3_ta_control.py
@@ -0,0 +1,169 @@
+import numpy as np
+import numba as nb
+import pymap3d as pm
+from lib_ta_py.controller_TA_v2 import Controller_v1
+import rospy
+from pkg_ta.msg import Control
+from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+
+freq = 20 # Hz
+waypoints_np = np.load('waypoints/waypoints/28_08_lurus.npy')
+# waypoints_np = np.load('waypoints/waypoints/28_08_belok.npy')
+# waypoints_np = np.load('waypoints/waypoints/28_08_S.npy')
+
+# In the Arduino, CW is positive and CCW is negative
+# On the other hand, in the controller algoritm, CCW is positive and CW is negative
+max_steer = 35.; min_steer = -28. # For the path following control algoritm ~ degree
+max_steer_arduino = 28.; min_steer_arduino = -35. # For the Arduino ~ degree
+max_brake = 2.9; max_throttle = 0.2; min_throttle = 0.08
+
+kp = 0.15; ki = 0.075; kd = 0.0
+ff_long = np.array([0.0, 0.0]) # no feed-forward
+ks = 0.75; kv = 2.25; kff_lat = 2.0; dead_band_limit = 0.01
+# kv_lat = 1.5; kv_yaw = 1.5; kv_throttle = 2.5
+kv_lat = 0.0; kv_yaw = 1.5; kv_throttle = 2.5 # UNTUK TUNING PID AJA
+kp_lat = 15. * np.pi / 180.
+kd_lat = 30. * np.pi / 180.
+sat_long = np.array([-np.abs(max_brake), np.abs(max_throttle)])
+sat_lat = np.array([-np.abs(min_steer), np.abs(max_steer)])
+sat_lat = sat_lat * np.pi / 180.
+
+# Reference point
+lat0, lon0, h0 = -6.8712, 107.5738, 768
+
+state = {'x': 0., 'y': 0., 'yaw': 0., 'v': 0.}
+
+RUN_imu = False # Tunggu sampai data yaw masuk
+RUN_gps = False # Tunggu sampai data posisi masuk
+RUN_filtered_map = False # Tunggu sampai data kecepatan masuk
+
+@nb.njit()
+def to_euler(x, y, z, w):
+    """Dari Coursera: Return as xyz (roll pitch yaw) Euler angles."""
+    roll = np.arctan2(2 * (w * x + y * z), 1 - 2 * (x**2 + y**2))
+    pitch = np.arcsin(2 * (w * y - z * x))
+    yaw = np.arctan2(2 * (w * z + x * y), 1 - 2 * (y**2 + z**2))
+    return np.array([roll, pitch, yaw])
+# Compile the to_euler
+_ = to_euler(1.5352300785980803e-15, -1.3393747145983517e-15, -0.7692164172827881, 0.638988343698562)
+
+
+last_time = 0.0
+def main():
+    global last_time
+
+    # Create the controller object
+    controller = Controller_v1(kp, ki, kd, ff_long, sat_long,
+                               ks, kv, kff_lat, dead_band_limit, sat_lat,
+                               waypoints_np,
+                               max_throttle, min_throttle,
+                               kv_yaw, kv_lat, kv_throttle,
+                               kp_lat, kd_lat)
+
+    rospy.init_node('control')
+
+    msg = Control()
+    msg.header.frame_id = 'path_following_control'
+    msg.header.seq = 0
+    msg.header.stamp = rospy.Time.now()
+    last_time = msg.header.stamp.to_sec() - 1./freq
+
+    def callback_gps(msg_gps):
+        global state
+        global RUN_gps
+        global last_time
+
+        gps_pos = pm.geodetic2enu(msg_gps.latitude,
+                                  msg_gps.longitude,
+                                  msg_gps.altitude, lat0, lon0, h0)
+        state['x'] = gps_pos[0]
+        state['y'] = gps_pos[1]
+
+        RUN_gps = True
+
+        # Calculate the actual sampling time
+        gps_stamp = msg_gps.header.stamp
+        delta_t = gps_stamp.to_sec() - last_time
+        last_time = gps_stamp.to_sec()
+
+        # Calculate the control signal
+        long, lat = controller.calculate_control_signal(delta_t, state['x'],
+                                                        state['y'], state['v'],
+                                                        state['yaw'])
+
+        msg.action_steer = max(min(-lat*180/np.pi, max_steer_arduino), min_steer_arduino) # lat ~ radian
+        #msg.action_throttle = max(min(long, max_throttle), min_throttle)
+        msg.action_throttle = max(min(long, max_throttle), 0.0)
+        #msg.action_throttle = 0.225
+        msg.action_brake = 0.
+
+    def callback_imu(msg_imu): # Callback IMU
+        global state
+        global RUN_imu
+
+        q = msg_imu.orientation
+        euler = to_euler(q.x, q.y, q.z, q.w)
+        state['yaw'] = euler[-1] * (-1)
+
+        RUN_imu = True
+
+    def callback_filtered_map(msg_fm):
+        global state
+        global RUN_filtered_map
+
+        vel = msg_fm.twist.twist.linear
+        state['v'] = np.sqrt(vel.x**2 + vel.y**2) # m/s
+
+        RUN_filtered_map = True
+
+    rospy.Subscriber('/fix', NavSatFix, callback_gps)
+    rospy.Subscriber('/imu', Imu, callback_imu)
+    rospy.Subscriber('/odometry/filtered_map', Odometry, callback_filtered_map)
+    pub = rospy.Publisher('/control_signal', Control, queue_size=1)
+    rate = rospy.Rate(freq) # Hz
+
+    print("Menunggu data gps, imu, dan kecepatan masuk pertama kali ...")
+    RUN = False
+    while not RUN:
+        RUN = RUN_gps and RUN_imu and RUN_filtered_map
+        pass # INI DI WHILE KALAU DATA GPS BELUM MASUK BUAT INISIALISASI
+    print("Data Navigasi sudah masuk !")
+    print("Program sudah berjalan !")
+
+    while not rospy.is_shutdown():
+        # Calculate the actual sampling time
+        msg.header.stamp = rospy.Time.now()
+
+        # Get the error profile
+        err = controller.get_error()
+        # Get the reference
+        ref = controller.get_instantaneous_setpoint()
+
+        # Send the message
+        msg.header.seq += 1
+        msg.error_speed = err[0]
+        msg.error_lateral = err[1]
+        msg.error_yaw = err[2]
+
+        msg.actual_x = state['x']
+        msg.actual_y = state['y']
+        msg.actual_yaw = state['yaw']
+        msg.actual_speed = state['v']
+
+        msg.wp_idx = controller.get_closest_index()
+        msg.ref_x = ref[0]
+        msg.ref_y = ref[1]
+        msg.ref_yaw = ref[2]
+        msg.ref_speed = ref[3]
+        msg.ref_curvature = ref[4]
+
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
